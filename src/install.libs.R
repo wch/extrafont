7763b4ba67a3a6c3d122b77b0c5d50240e148d48
Winston Chang <winston@stdout.org>
2012-05-20 17:01:31 -0500

    Added makefile and scripts to install ttf2pt1

---
+++ src/install.libs.R
@@ -0,0 +1,10 @@
+# Copy ttf2pt1/ttf2pt1 or ttf2pt1/ttf2pt1.exe binary
+files <- file.path("ttf2pt1", if (WINDOWS) "tt2fp21.exe" else "ttf2pt1")
+
+# Default installation (from R extensions doc)
+libarch <- if (nzchar(R_ARCH)) paste('libs', R_ARCH, sep='') else 'libs'
+dest <- file.path(R_PACKAGE_DIR, libarch)
+
+message("Installing ", files, " to ", dest)
+dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+file.copy(files, dest, overwrite = TRUE)
