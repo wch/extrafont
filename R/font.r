766acd7b3f5b74bf327c5a724b4ca60f2053f1a8
Winston Chang <winston@stdout.org>
2012-05-20 22:34:31 -0500

    Fix wrong function name in message

--- R/font.r
+++ R/font.r
@@ -23,7 +23,7 @@ fonts <- function() {
 #' @export
 font_load_table <- function() {
   if (!file.exists(font_table_file()))
-    stop("Can't find ", font_table_file(), ". Have you run import_ttf()?")
+    stop("Can't find ", font_table_file(), ". Have you run ttf_import()?")
 
   read.csv(font_table_file(), stringsAsFactors = FALSE)
 }
