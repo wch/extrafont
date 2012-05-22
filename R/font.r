c7a6f4f75ea830d1c68dbd47b1de4da09faebadb
Winston Chang <winston@stdout.org>
2012-05-21 23:19:51 -0500

    Add font_install()

--- R/font.r
+++ R/font.r
@@ -8,12 +8,53 @@ font_import <- function(paths = NULL, recursive = TRUE, prompt = TRUE) {
 }
 
 
-#' Import font from an installed package
+#' Install a font package and register it with extrafont
+#'
+#' If the font package specified by \code{fontpkg} is not already installed,
+#' it will be downloaded from CRAN. The font package will then be registered
+#' with extrafont.
+#'
+#' @param fontpkg The name of an R package containing a font.
+#'
+#' @export
+font_install <- function(fontpkg = NULL) {
+  if (is.null(fontpkg))
+    stop("fontpkg must be specified.")
+
+  # Check if font package already installed
+  if (is.element(fontpkg, installed.packages()[,1])) {
+    message('Package "', fontpkg, '" already installed.')
+  } else {
+    # Not installed; try to install from cran
+    message('Package "', fontpkg, '" not installed. Attempting to install from CRAN...')
+    install.packages(fontpkg)
+
+    if (!is.element(fontpkg, installed.packages()[,1]))
+      stop('Package "', fontpkg, '" not successfully installed.')
+  }
+
+
+  # Check if font needs to be registered with extrafont
+  # (and do it if needed.)
+  message('Registering font package "', fontpkg, '" with extrafont.')
+  font_addpackage(fontpkg)
+
+}
+
+
+#' Add font from an installed package to extrafont's registry
 #'
 #' @param pkg The name of the font package, e.g., \code{"fontcm"}.
 #' @export
 font_addpackage <- function(pkg = NULL) {
   if(is.null(pkg)) stop("No package specified.")
+
+  # Check if font package already added
+  ft <- fonttable_load()
+  if (any(ft$package == pkg)) {
+    message('Font package "', pkg, '" already registered with extrafont.')
+    return(invisible())
+  }
 
   pkgdir <- system.file(package = pkg)
 
@@ -31,5 +72,3 @@ font_addpackage <- function(pkg = NULL) {
   }
 
 }
-
-
